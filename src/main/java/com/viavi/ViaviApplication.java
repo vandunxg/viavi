d1d424a4ba5ddb2f90563ac46fb43bfeb6f9da1c
vandunxg <nguyenvandung.official@gmail.com>
2024-08-13 22:06:06 +0700

    init project

---
+++ src/main/java/com/viavi/ViaviApplication.java
@@ -0,0 +1,13 @@
+package com.viavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaviApplication.class, args);
+	}
+
+}
