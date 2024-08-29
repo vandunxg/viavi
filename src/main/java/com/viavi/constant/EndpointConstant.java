f2521c083397516671ab449b41f7b1799f5bf219
Nguyen Van Dung <nguyenvandung.official@gmail.com>
2024-08-29 10:32:19 +0700

    feat : add comment and "add" endpoint

--- src/main/java/com/viavi/constant/EndpointConstant.java
+++ src/main/java/com/viavi/constant/EndpointConstant.java
@@ -2,7 +2,11 @@ package com.viavi.constant;
 
 public class EndpointConstant {
 
+    // auth
     public static final String ENDPOINT_REGISTER = "/register";
     public static final String ENDPOINT_LOGIN = "/login";
+
+    // CRUD
+    public static final String ENDPOINT_ADD = "/add";
 
 }
