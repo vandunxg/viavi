9e3ea2a0626afe5485b8bb23bdd9ab9302237102
Nguyen Van Dung <nguyenvandung.official@gmail.com>
2024-08-29 10:34:08 +0700

    feat : init PermissionException

---
+++ src/main/java/com/viavi/exception/PermissionException.java
@@ -0,0 +1,12 @@
+package com.viavi.exception;
+
+import com.viavi.utils.ErrorCode;
+
+
+public class PermissionException extends GlobalException {
+
+    public PermissionException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+}
